0f1abd0f1b06faa4d34f0c5fb0227ab8c9395963
Gaurav Joshi <gj451026@gmail.com>
2022-07-28 17:07:19 +0530

    Navbar added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+
+import Instagram_logo from "../../assets/Instagram_logo.webp";
+
+import { GrHomeRounded } from "react-icons/gr";
+import {IoPaperPlaneOutline} from 'react-icons/io5'
+import {BsCircle, BsHeart} from 'react-icons/bs'
+import {CgAddR} from 'react-icons/cg'
+
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <navigator>
+      <div className="Navbar_container">
+        <div className="Navbar_container-Box1">
+          <div className="Navbar_container-Box1-logo">
+            <img src={Instagram_logo} alt="Instagram_logo" />
+          </div>
+        </div>
+        <div className="Navbar_container-Box2">
+          <div className="Navbar_container-Box2-search">
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+        <div className="Navbar_container-Box3">
+          <ul className="Navbar_container-Box3-icons">
+            <li> <a href="/"><GrHomeRounded /></a>  </li>
+            <li> <a href="/"><IoPaperPlaneOutline/> </a></li>
+            <li> <a href="/"><CgAddR/> </a></li>
+            <li> <a href="/"><BsCircle/></a> </li>
+            <li> <a href="/"><BsHeart /></a>  </li>
+          </ul>
+        </div>
+      </div>
+    </navigator>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+import insta_background from '../../assets/insta_background.png'
+
+import './Home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <div className="home_background">
+        <img src={insta_background} alt="" />
+      </div>
+    </div>
   )
 }
 
