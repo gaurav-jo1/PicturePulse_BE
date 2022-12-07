287b4966007a98d6ec3d84851f17be537ea198c3
Gaurav <gj451026@gmail.com>
2022-12-07 11:55:08 +0530

    Strict mode added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,6 @@ import client from './react-query-client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <BrowserRouter>
