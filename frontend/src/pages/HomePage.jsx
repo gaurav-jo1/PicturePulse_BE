e54347bd90bcb05d28e9fc860ef4d5b4b2a4b717
Gaurav <gj451026@gmail.com>
2022-12-08 16:13:01 +0530

    Home page styling added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React, { useContext } from "react";
-// import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { ThemeContext } from "../context/ThemeContextProvider";
 import { AuthContext } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+import {BsThreeDots,BsHeart,BsHeartFill} from "react-icons/bs"
+import {TbMessageCircle2} from "react-icons/tb"
+import {IoIosShareAlt} from "react-icons/io"
 import Navbar from "../components/Navbar";
 import no_profile from "../assets/35-The-Beauty-of-Anya-Forger.png";
+import girl from "../assets/istockphoto-1220780604-612x612.jpg"
 import "../styling/HomePage.scss";
 
 const HomePage = () => {
@@ -22,9 +26,6 @@ const HomePage = () => {
   const { data: userinfos } = useQuery(["userinfos"], () => {
     return getInfo("http://127.0.0.1:8000/userinfo/").then((t) => t.json());
   });
-  // const { data: images } = useQuery(["googleImages"], () => {
-  //   return axios.get("https://serpapi.com/playground?q=Apple").then((t) => t.json());
-  // });
 
   const { theme } = useContext(ThemeContext);
 
@@ -36,21 +37,29 @@ const HomePage = () => {
       <div className="HomePage_container_div">
         <div className="HomePage_container_Images-container">
           <div className="HomePage_container_Images-user">
-            <div className="HomePage_container_Images-user_profile">
-              <img src="" alt="" />
+            <div className="HomePage_container_Images-user_profile-container">
+              <div className="HomePage_container_Images-user_profile">
+                <img src={girl} alt="girl"  width="40" height="40"/>
+              </div>
+              <div className="HomePage_container_Images-user_username">
+                <p>Gaurav Joshi</p>
+                <p>10 min ago</p>
+              </div>
             </div>
-            <div className="HomePage_container_Images-user_username">
-              <p>Gaurav Joshi</p>
+            <div className="HomePage_container_Images-user_threedot">
+              <p><BsThreeDots/></p>
             </div>
           </div>
           <div className="HomePage_container_Images-img">
-            <img src="https://picsum.photos/seed/picsum/500/300" alt="" />
+            <img src="https://picsum.photos/seed/picsum/700/400" alt="" />
           </div>
           <div className="HomePage_container_Images-reaction">
             <ul>
-              <li>like</li>
-              <li>Date</li>
-              <li>share</li>
+              <div className="HomePage_container_image_react_icons">
+                <li><BsHeart/></li>
+                <li><TbMessageCircle2/></li>
+              </div>
+              <li><IoIosShareAlt/></li>
             </ul>
           </div>
         </div>
@@ -62,7 +71,7 @@ const HomePage = () => {
                 {userinfo.picture ? (
                   <img src={`http://127.0.0.1:8000/${userinfo.picture}`} alt={userinfo.user} width="60" height="60"/>
                 ) : (
-                  <img src={no_profile} alt="no profile" width="30" height="30"/>
+                  <img src={no_profile} alt="no profile" width="60" height="60"/>
                 )}
               </div>
               <div className="HomePage_container_Profile_username">
@@ -72,6 +81,7 @@ const HomePage = () => {
             </div>
             <div className="HomePage_container_Profile_bio-container">
               <p>{userinfo.profession}</p>
+              <p><Link to="/profile"><strong>View Profile</strong></Link></p>
             </div>
           </div>
         ))}
