102f14b6fa97ab772dab3008e7571a1d04026d15
Gaurav <gj451026@gmail.com>
2023-01-05 09:49:53 +0530

    Key added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +24,7 @@ REAL_BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)q1gp%ezwm(de9p6e&=e(a@pm1g6(dkw3gnxjtq)e7etvvvf8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
