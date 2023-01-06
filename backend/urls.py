15f2b021dcac843863024829df833d9e6ba24c2f
Gaurav Joshi <gj451026@gmail.com>
2023-01-06 10:49:56 +0530

    Settings updated

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('userProfile.urls')),
-]
-
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
