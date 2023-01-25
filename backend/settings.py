996d5c846ec98269a99a8145b2bf278f6dbb06a2
Gaurav Joshi <93304640+Gaurav-jo1@users.noreply.github.com>
2023-01-25 22:18:32 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qkp^_#&x0t#7c(ytz9ww+z*9n%rey#7o3993(mbp))$ezht2h#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["picturepulsebackend.up.railway.app","picturepulse.up.railway.app","localhost"]
 
